5a69db91e68d5487dc890a1b437ac2f134e63b55
Rich Lott <forums@artfulrobot.uk>
2016-10-15 08:58:20 +0100

    refactor webhook run to allow for testing

--- CRM/Gocardlessdd/Page/Webhook.php
+++ CRM/Gocardlessdd/Page/Webhook.php
@@ -7,30 +7,50 @@
 require_once 'CRM/Core/Page.php';
 
 class CRM_Gocardlessdd_Page_Webhook extends CRM_Core_Page {
+
+  /** @var bool */
+  protected $test_mode;
+
   public function run() {
 
-    $data_raw = file_get_contents("php://input");
-    $data = json_decode($data_raw);
-
-    $token = getenv("GC_WEBHOOK_SECRET");
-
+    // We need to check the input against the test and live payment processors.
     $raw_payload = file_get_contents('php://input');
+    // debugging:
+    file_put_contents('/tmp/gcwebhook-' . date('Y-m-d-His'), $raw_payload);
 
     $headers = getallheaders();
-    $provided_signature = $headers["Webhook-Signature"];
 
-    $calculated_signature = hash_hmac("sha256", $raw_payload, $token);
-
-    if ($provided_signature == $calculated_signature) {
-      // Process the events
-
-      header("HTTP/1.1 200 OK");
-    } else {
+    if (!$this->checkWebhookSource($headers, $raw_payload)) {
+      // Invalid webhook call.
       header("HTTP/1.1 498 Invalid Token");
+      CRM_Utils_System::civiExit();
     }
 
-        //CRM_Utils_System::civiExit();
-          //CRM_Core_Error::Fatal("ERROR: Stripe triggered a Webhook on an invoice not found in civicrm_contribution_recur: " . $stripe_event_data);
+    // Process the events
+    header("HTTP/1.1 200 OK");
+    CRM_Utils_System::civiExit();
+
+    //CRM_Core_Error::Fatal("ERROR: Stripe triggered a Webhook on an invoice not found in civicrm_contribution_recur: " . $stripe_event_data);
     parent::run();
+  }
+
+  /**
+   * Check incomming input for validity.
+   */
+  public function checkWebhookSource($headers, $raw_payload) {
+
+    $provided_signature = isset($headers["Webhook-Signature"]) ? $headers["Webhook-Signature"] : '';
+    $valid = FALSE;
+    foreach([FALSE, TRUE] as $test) {
+      $pp = CRM_GoCardlessUtils::getPaymentProcessor($test);
+      $token = $pp['signature'];
+      $calculated_signature = hash_hmac("sha256", $raw_payload, $token);
+      if ($token && $provided_signature == $calculated_signature) {
+        $valid = TRUE;
+        $this->test_mode = $test;
+        break;
+      }
+    }
+
   }
 }
