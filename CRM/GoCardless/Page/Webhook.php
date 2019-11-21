28abf9d47c4e666548da65a563d80509cffee809
Rich Lott / Artful Robot <forums@artfulrobot.uk>
2019-11-21 16:57:56 +0000

    Another aproach to fixing repeattransaction

--- CRM/GoCardless/Page/Webhook.php
+++ CRM/GoCardless/Page/Webhook.php
@@ -155,6 +155,7 @@ class CRM_GoCardless_Page_Webhook extends CRM_Core_Page {
     }
   }
 
+
   /**
    * Process webhook for 'payments' resource type, action 'confirmed'.
    *
@@ -171,9 +172,57 @@ class CRM_GoCardless_Page_Webhook extends CRM_Core_Page {
     $payment = $this->getAndCheckGoCardlessPayment($event, ['confirmed', 'paid_out']);
     $recur = $this->getContributionRecurFromSubscriptionId($payment->links->subscription);
 
-    // Ensure that the recurring contribution record is set to In Progress.
-    // ...but only if it's currently Pending or Overdue.
-    // ...AND check that it has not been cancelled.
+    $this->updateContributionRecurRecord($recur, $payment);
+
+    // Prepare to update the contribution records.
+    $contribution = [
+      'total_amount'           => number_format($payment->amount / 100, 2, '.', ''),
+      'receive_date'           => $payment->charge_date,
+      'trxn_date'              => $payment->charge_date,
+      'trxn_id'                => $payment->id,
+      'contribution_recur_id'  => $recur['id'],
+      'financial_type_id'      => $recur['financial_type_id'],
+      'contact_id'             => $recur['contact_id'],
+      'is_test'                => $this->test_mode ? 1 : 0,
+    // Do not send email receipts. This might annoy some people. Be nice if it was a setting.
+      'is_email_receipt'       => 0,
+    ];
+    // Note: the param called 'trxn_date' which is used for membership date
+    // calculations. If it's not given, today's date gets used.
+
+    $pending_contribution_id = $this->getPendingContributionId($recur);
+    if ($pending_contribution_id) {
+      // There's an existing pending contribution.
+      $contribution['id'] = $pending_contribution_id;
+
+      // Update the contribution.
+      civicrm_api3('Contribution', 'create', $contribution);
+
+      // Now call completetransaction. Note that the only data this updates in
+      // the contribution record is trxn_id and fee_amount (which we don't
+      // supply).
+      civicrm_api3('Contribution', 'completetransaction', $contribution);
+      // We're done here.
+      return;
+    }
+
+    // This is another payment after the first.
+    $this->handleRepeatTransaction($contribution, $recur);
+  }
+
+  /**
+   * Ensure that the recurring contribution record is set to In Progress.
+   * ...but only if it's currently Pending or Overdue.
+   * ...AND check that it has not been cancelled.
+   *
+   * @todo
+   * There's all sorts of other fields on recur - do we need to set them?
+   * e.g. date of next expected payment, date of last update etc.
+   *
+   * @param array $recur
+   * @param array $payment
+   */
+  public function updateContributionRecurRecord($recur, $payment) {
     $contrib_recur_statuses = CRM_Contribute_BAO_ContributionRecur::buildOptions('contribution_status_id', 'validate');
     if (in_array($contrib_recur_statuses[$recur['contribution_status_id']], ['Pending', 'Overdue'])) {
       // It would be Overdue if the last payment failed.
@@ -201,24 +250,16 @@ class CRM_GoCardless_Page_Webhook extends CRM_Core_Page {
       ]);
     }
 
-    // There's all sorts of other fields on recur - do we need to set them?
-    // e.g. date of next expected payment, date of last update etc. @todo
+  }
+  /**
+   *
+   * @param Array $contribution
+   * @param Array $recur
+   */
+  public function handleRepeatTransaction($contribution, $recur) {
 
-    // Prepare to update the contribution records.
-    $contribution = [
-      'total_amount'           => number_format($payment->amount / 100, 2, '.', ''),
-      'receive_date'           => $payment->charge_date,
-      'trxn_date'              => $payment->charge_date,
-      'trxn_id'                => $payment->id,
-      'contribution_recur_id'  => $recur['id'],
-      'financial_type_id'      => $recur['financial_type_id'],
-      'contact_id'             => $recur['contact_id'],
-      'is_test'                => $this->test_mode ? 1 : 0,
-    // Do not send email receipts. This might annoy some people. Be nice if it was a setting.
-      'is_email_receipt'       => 0,
-    ];
-    // Note: the param called 'trxn_date' which is used for membership date
-    // calculations. If it's not given, today's date gets used.
+    // There is no pending contribution, find the original one.
+    $contribution['original_contribution_id'] = $this->getOriginalContributionId($recur);
 
     // From CiviCRM 5.19.1 (and possibly earlier) we need to specify the
     // membership_id on the contribution, otherwise the membership does not get
@@ -230,31 +271,66 @@ class CRM_GoCardless_Page_Webhook extends CRM_Core_Page {
     if ($memberships['count'] == 1) {
       $contribution['membership_id'] = $memberships['values'][0]['id'];
     }
+    // Contributions need a unique invoice_id. We won't have this yet, so we use trxn_id.
+    $contribution['invoice_id'] = $contribution['trxn_id'];
+    // We'll copy various fields from the original, needed apparently.
+    if (!empty($contribution['original_contribution_id'])) {
+      $orig = civicrm_api3('Contribution', 'getsingle', ['id' => $contribution['original_contribution_id']]);
+    }
+    $contribution['source'] = $orig['source'] ?? '';
 
-    $pending_contribution_id = $this->getPendingContributionId($recur);
-    if ($pending_contribution_id) {
-      // There's an existing pending contribution.
-      $contribution['id'] = $pending_contribution_id;
+    // Apparently we might need to correct this.
+    $contribution['payment_instrument_id'] = $this->payment_processor->getPaymentInstrumentID();
 
-      // Update the contribution.
-      $result = civicrm_api3('Contribution', 'create', $contribution);
+    // Create a copy record of the original contribution.
+    $contribution['contribution_status_id'] = 'Pending';
+    $result = civicrm_api3('Contribution', 'repeattransaction', $contribution);
 
-      // Now call completetransaction. Note that the only data this updates in
-      // the contribution record is trxn_id and fee_amount (which we don't
-      // supply).
-      $result = civicrm_api3('Contribution', 'completetransaction', $contribution);
-      // We're done here.
+    if (!$result['id']) {
+      CRM_Core_Error::debug_log_message(
+        "Webhook $this->now Failed event. repeattransaction did not result in a new contribution.",
+        FALSE, 'GoCardless', PEAR_LOG_ERR);
       return;
     }
 
-    // There is no pending contribution, find the original one.
-    $contribution['original_contribution_id'] = $this->getOriginalContributionId($recur);
-    $contribution['contribution_status_id'] = 'Completed';
+    // repeattransaction worked.
+    $contribution['id'] = $result['id'];
 
-    // Create a copy record of the original contribution and send out email receipt
-    $result = civicrm_api3('Contribution', 'repeattransaction', $contribution);
+    // First restore/add various fields that the repeattransaction api may overwrite or ignore.
+    // This is a blatant copy from:
+    // https://github.com/iATSPayments/com.iatspayments.civicrm/blob/8901211fe6dba85430c92abf7bd0b45ab552a4c2/CRM/Iats/Transaction.php#L80
+    $params = [
+      'id'                    => $contribution['id'],
+      'invoice_id'            => $contribution['invoice_id'],
+      'receive_date'          => $contribution['receive_date'],
+      'source'                => $contribution['source'],
+      'payment_instrument_id' => $contribution['payment_instrument_id'],
+    ];
+    $result = civicrm_api3('Contribution', 'create', $params);
+    if ($result['is_error'] ?? NULL) {
+      CRM_Core_Error::debug_log_message(
+        "Webhook $this->now Failed event. repeattransaction worked but trying to correct the data it fails on did not.",
+        $params, 'GoCardless', PEAR_LOG_ERR);
+      return;
+    }
+
+    // Now complete the transaction.
+    $params = [
+      'id'                   => $contribution['id'],
+      'payment_processor_id' => $this->payment_processor->getPaymentProcessor()['id'],
+      'is_email_receipt'     => (empty($contribution['is_email_receipt']) ? 0 : 1),
+      'trxn_id'              => $contribution['trxn_id'],
+      'receive_date'         => $contribution['receive_date'],
+    ];
+    $result = civicrm_api3('Contribution', 'completetransaction', $params);
+    if ($result['is_error'] ?? NULL) {
+      CRM_Core_Error::debug_log_message(
+        "Webhook $this->now Failed event. completetransaction failed.",
+        $params, 'GoCardless', PEAR_LOG_ERR);
+      return;
+    }
+
   }
-
   /**
    * Process webhook for 'payments' resource type, action 'failed'.
    */
